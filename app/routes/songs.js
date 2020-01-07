b7a3df165c42e998126e6b8832361be6f272a143
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-07 11:50:00 +0100

    adding routs bands and songs + navigation bar

--- app/routes/songs.js
+++ app/routes/songs.js
@@ -1,12 +1,13 @@
 import Route from '@ember/routing/route';
 import EmberObject from '@ember/object';
-import { A } from '@ember/array'
+import { A } from '@ember/array';
 
 let Song = EmberObject.extend({
     title: '',
     band: '',
     rating: 0
 });
+
 
 export default Route.extend({
     model() {
