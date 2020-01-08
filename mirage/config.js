913413d3d6b0a1ebc5d5bb54bdabbf0ff1d8b7e4
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-08 17:07:39 +0100

    setup test for in prep for query param impl.

--- mirage/config.js
+++ mirage/config.js
@@ -1,5 +1,9 @@
-export default function() {
+export default function () {
   this.get('/bands');
   this.post('/bands');
   this.get('/bands/:id');
+  this.get('/bands/:id/songs', function (schema, request) {
+    let id = request.params.id;
+    return schema.songs.where({bandId: id});
+  });
 }
