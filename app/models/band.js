f9ad850d7c37e9eb696636c5a1f8152fd8c77454
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-08 11:52:57 +0100

    updated EmberObject classes Band and Song to Models.

--- app/models/band.js
+++ app/models/band.js
@@ -1,13 +1,12 @@
-import EmberObject from '@ember/object';
+import Model, {attr, hasMany} from '@ember-data/model';
 
-export default EmberObject.extend({
-    name: '',
-    description: '',
-
-    init() {
-        this._super(...arguments);
-        if (!this.songs) {
-            this.set('songs', []);
-        }
-    }
+/**
+ * no {async: false} means it's considered an async relationship
+ */
+export default Model.extend({
+  name: attr(), // specifies that it is an attribute of the model
+  description: attr(), // no param defaults to string???
+  // links to the model class Song
+  // !!!don't need to type 'song', can be inferred from the key!!!
+  songs: hasMany('song')
 });
