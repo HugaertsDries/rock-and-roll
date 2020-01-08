464f9e66a77d432153513800dab89c8b01b32069
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-08 10:36:01 +0100

    resetting bands.band.song controller.

--- app/routes/bands/band/songs.js
+++ app/routes/bands/band/songs.js
@@ -1,6 +1,13 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+    resetController(controller) {
+      controller.setProperties({
+        isAddingSong: false,
+        newSongTitle: '',
+      })
+    },
+
     model() {
         let band = this.modelFor('bands.band');
         return band;
