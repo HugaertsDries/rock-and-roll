6232e3f25aa00c65f68320d2715a8d60e08788bd
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-01-08 11:00:07 +0100

    adding dynamic doc titles (could be done with an addon).

--- app/router.js
+++ app/router.js
@@ -3,7 +3,38 @@ import config from './config/environment';
 
 const Router = EmberRouter.extend({
   location: config.locationType,
-  rootURL: config.rootURL
+  rootURL: config.rootURL,
+
+  // constructor
+  init() {
+    this._super();
+    this.setDocumentTitle();
+  },
+
+  setDocumentTitle() {
+    // EVENT LISTENER
+    this.on('routeDidChange', (transition) => {
+      let toRouteName = transition.to.name;
+      let pageTitles = {
+        'bands.index': () => {
+          return 'Bands';
+        },
+        'bands.band.songs': () => {
+          let bandRouteInfo = transition.to.find(info => info.name.includes('bands.band'));
+          let bandSlug = bandRouteInfo.params.slug;
+          let bandName = bandSlug.split('-').map(s => s.capitalize()).join(' ');
+          return `${bandName} songs`;
+        }
+      };
+      let titleSegments = [];
+      let titleSetter = pageTitles[toRouteName];
+      if (titleSetter) {
+        titleSegments.push(titleSetter());
+      }
+      titleSegments.push('Rock and Roll with Ember.js');
+      document.title = titleSegments.join(' - ');
+    });
+  }
 });
 
 Router.map(function () {
@@ -11,13 +42,12 @@ Router.map(function () {
   this.route('bands', function () {
     // bands.band
     // dynamic route, has a slug
-    this.route('band', { path: ':slug' }, function () {
+    this.route('band', {path: ':slug'}, function () {
       // bands.band.songs
       this.route('songs');
       this.route('details');
     });
   });
-
 });
 
 export default Router;
